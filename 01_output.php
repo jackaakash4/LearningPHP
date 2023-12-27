6edf09d6f200a63ec021f946f70ac615e8fd81c4
jackaakash4 <jackaakash4@gmail.com>
2023-12-27 22:22:15 +0545

    Feat: First php code

---
+++ 01_output.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 1,' ', 'Hello echo ', 3.14;
+
+// print 'Hello print';
+
+//print_r() -> print single values and arrays
+print_r([1, 2, 3]);
+print_r(['Aakash', " is ", "learning ", "php"])
+
+?>
